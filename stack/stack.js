759ebbdbec7e13cdfe52f9487dad7014a4192819
davidliberal0 <davidliberal0@gmail.com>
2022-07-28 23:07:41 -0400

    add stack

---
+++ stack/stack.js
@@ -0,0 +1,36 @@
+class Stack {
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+
+  // add element to the top of stack
+  push(element) {
+    // add element according to the count (position)
+    this.items[this.count] = element;
+    console.log(`${element} added to ${this.count}`);
+    // increment count on each usage to add number to the end of the list
+    this.count += 1;
+    return this.count - 1;
+  }
+
+  // return and remove the top element in stack
+  // return undefined if stack is empty
+  pop() {
+    if (this.count == 0) {
+      return undefined;
+    }
+
+    let deleteItem = this.items[this.count - 1];
+    this.count -= 1;
+    console.log(`${deleteItem} removed`);
+    return deleteItem;
+  }
+}
+
+// create stack
+const stack = new Stack();
+
+stack.push(100);
+stack.push(200);
+stack.push(300);
